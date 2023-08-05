95b28ebccc442825f1ee44dd183347a27ed83020
Erojikaruna <erojikaruna@gmail.com>
2023-08-05 13:21:32 +0530

    updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 export const LOGO_URL =
-  "https://st2.depositphotos.com/1351697/8470/v/950/depositphotos_84709916-stock-illustration-food-and-restaurant-logo-design.jpg";
+  "https://img.lovepik.com/element/45016/8577.png_860.png";
 export const MENU_URL =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.385044&lng=78.486671&restaurantId=";
